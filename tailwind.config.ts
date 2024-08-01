e7aa7937be8aaa9226dff13b63f83f27b5141a43
houndhollis <houndhollis9797@gmail.com>
2024-08-01 19:17:40 +0900

    feat: 태일윈드 설정 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,48 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "516px",
+      md: "1016px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        gray: {
+          100: "#2f3336",
+          600: "#E7E9EA",
+        },
+        blue: {
+          500: "#1d9bf0",
+        },
+      },
+      fontSize: {
+        6: "0.375em",
+        7: "0.438em",
+        8: "0.500em",
+        9: "0.563em",
+        10: "0.625em",
+        11: "0.688em",
+        12: "0.750em",
+        13: "0.813em",
+        14: "0.875em",
+        15: "0.938em",
+        16: "1.000em",
+        18: "1.125em",
+        20: "1.250em",
+        22: "1.375em",
+        24: "1.500em",
+      },
+      borderRadius: {
+        4: "4px",
+        8: "8px",
+        12: "12px",
+        16: "16px",
+        20: "20px",
       },
     },
   },
