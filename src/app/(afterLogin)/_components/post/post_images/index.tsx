bc75b33d5c85d4ed488ad525913f170a25e8d5c9
houndhollis <houndhollis9797@gmail.com>
2024-09-12 14:15:23 +0900

    feat: 이미지 컴포넌트 추가

---
+++ src/app/(afterLogin)/_components/post/post_images/index.tsx
@@ -0,0 +1,128 @@
+import Link from "next/link";
+import Image from "next/image";
+import style from "./post.images.module.css";
+import clsx from "clsx";
+
+type Props = {
+  post: {
+    postId: number;
+    content: string;
+    User: {
+      id: string;
+      nickname: string;
+      image: string;
+    };
+    createdAt: Date;
+    Images: any[];
+  };
+};
+
+export default function PostImages({ post }: Props) {
+  if (!post.Images) return null;
+  if (!post.Images.length) return null;
+
+  if (post.Images.length === 1) {
+    return (
+      <Link
+        href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+        className={clsx(style.postImageSection, style.oneImage)}
+        style={{
+          backgroundImage: `url(${post.Images[0]?.link})`,
+          backgroundSize: "contain",
+        }}
+      >
+        <Image
+          width={300}
+          height={300}
+          src={post.Images[0]?.link}
+          alt="강원 사진"
+        />
+      </Link>
+    );
+  }
+
+  if (post.Images.length === 2) {
+    return (
+      <div className={clsx(style.postImageSection, style.twoImage)}>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+          style={{
+            backgroundImage: `url(${post.Images[0]?.link})`,
+            backgroundSize: "cover",
+          }}
+        />
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+          style={{
+            backgroundImage: `url(${post.Images[1]?.link})`,
+            backgroundSize: "cover",
+          }}
+        />
+      </div>
+    );
+  }
+  if (post.Images.length === 3) {
+    return (
+      <div className={clsx(style.postImageSection, style.threeImage)}>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+          style={{
+            backgroundImage: `url(${post.Images[0]?.link})`,
+            backgroundSize: "cover",
+          }}
+        />
+        <div>
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+            style={{
+              backgroundImage: `url(${post.Images[1]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[2].imageId}`}
+            style={{
+              backgroundImage: `url(${post.Images[2]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+  if (post.Images.length === 4) {
+    return (
+      <div className={clsx(style.postImageSection, style.fourImage)}>
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+          style={{
+            backgroundImage: `url(${post.Images[0]?.link})`,
+            backgroundSize: "cover",
+          }}
+        />
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+          style={{
+            backgroundImage: `url(${post.Images[1]?.link})`,
+            backgroundSize: "cover",
+          }}
+        />
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[2].imageId}`}
+          style={{
+            backgroundImage: `url(${post.Images[2]?.link})`,
+            backgroundSize: "cover",
+          }}
+        />{" "}
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[3].imageId}`}
+          style={{
+            backgroundImage: `url(${post.Images[3]?.link})`,
+            backgroundSize: "cover",
+          }}
+        />
+      </div>
+    );
+  }
+  return;
+}
